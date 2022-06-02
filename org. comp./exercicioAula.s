a3b73d4053d7dd6cbc167f03e7553df3761b0614
kevin willian <kevin_willian@hotmail.com.br>
2022-06-01 22:01:12 -0300

    aula 01052022

--- org. comp./exercicioAula.s
+++ org. comp./exercicioAula.s
@@ -9,10 +9,10 @@ S:	.word 0
 
 	.text
 main:
-	lw t1,A
-	lw t2,B
-	lw t3,C
-	lw t4,D
+	lw t1,A		#12
+	lw t2,B		#23
+	lw t3,C		#43
+	lw t4,D		#23
 	sub s1,t3,t2	#C-B
 	sub s2,t1,t3	#A-C
 	mul s3,s1,t4	#(A-C)*B
